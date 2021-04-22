c0fa4cb708fb77b9e0c92ffec70259f548efc9ec
edurso <eric@casadurso.com>
2021-04-22 14:03:41 -0400

    fix javadoc errors

--- src/main/java/com/lightningrobotics/quasar/Main.java
+++ src/main/java/com/lightningrobotics/quasar/Main.java
@@ -10,18 +10,18 @@ package com.lightningrobotics.quasar;
 import edu.wpi.first.wpilibj.RobotBase;
 
 /**
- * Do NOT add any static variables to this class, or any initialization at all.
- * Unless you know what you are doing, do not modify this file except to
- * change the parameter class to the startRobot call.
+ * Robot Application Driver Class.
+ * 
+ * <p>Starts Robot Application.
  */
 public final class Main {
     private Main() {
     }
 
     /**
-     * Main initialization function. Do not perform any initialization here.
-     *
-     * <p>If you change your main robot class, change the parameter type.
+     * Main initialization function. 
+     * 
+     * @param args Command line arguments
      */
     public static void main(String... args) {
         RobotBase.startRobot(Robot::new);
